9ed4a9401b0baa8df400639838a046877b670876
mkokosinski <mkokosinski91@gmail.com>
2022-02-06 18:08:02 +0100

    added routingd and error, 404 page sketch

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { DataProvider } from './context/dataContext';
 
 import './styles/index.css';
+import Page404 from './components/404/Page404';
+import ErrorPage from './components/errorPage/ErrorPage';
 
 ReactDOM.render(
   <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/error' element={<ErrorPage />} />
+        <Route path='*' element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
