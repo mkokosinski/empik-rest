a03dd1a650aa88b1496829748ac38eed3ef21abf
mkokosinski <mkokosinski91@gmail.com>
2022-02-06 17:46:57 +0100

    connect app to data provider

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useEffect } from 'react';
+import api, { endponits } from './api/api';
+import productApi from './api/productApi';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    productApi
+      .get()
+      .then((res) => {
+        console.log({ res });
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
