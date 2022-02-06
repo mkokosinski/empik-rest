4c0d0dde0404e6df6f7e07bf070c44e549c8e3fa
mkokosinski <mkokosinski91@gmail.com>
2022-02-06 19:59:05 +0100

    added error page

--- src/components/errorPage/ErrorPage.js
+++ src/components/errorPage/ErrorPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ErrorPage = (props) => {
-  return <div>ERROR</div>;
+import './errorPage.scss';
+
+const ErrorPage = ({ error }) => {
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+
+  return (
+    <div className='errorPage'>
+      <div>Ups, We have a problem:</div>
+
+      <div>{error}</div>
+
+      <button onClick={refreshPage}>Try Again</button>
+    </div>
+  );
 };
 
-ErrorPage.propTypes = {};
+ErrorPage.propTypes = {
+  error: PropTypes.string,
+};
 
 export default ErrorPage;
